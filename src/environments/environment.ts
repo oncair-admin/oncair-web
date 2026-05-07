892a04aaf88d8fed88ea8c7dfa414a9a2660b782
mohabsherif <mohab.barca@gmail.com>
2026-05-08 02:56:27 +0300

    chore: add apiUrl to environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  apiUrl: 'http://173.208.167.153:4443/',
   signalR: {
     hubUrl: 'http://localhost:5230/hubs/chat', // Backend SignalR hub URL
     notificationHubUrl: 'https://mob.acwade.com/chatHub', // Notification hub URL
